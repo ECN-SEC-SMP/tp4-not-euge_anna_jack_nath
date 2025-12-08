6624a80dcdf21daf86f0d327226dcb00e87bc69a
Annabelle <annabelle.goter@eleves.ec-nantes.fr>
2025-12-08 17:05:43 +0100

    ZA

--- test/ZATests.cpp
+++ test/ZATests.cpp
@@ -6,11 +6,12 @@ void ZATests() {
     // Test parcelle ZA
     Polygone<int>* forme = new Polygone<int>();
     ZA parcelle1 = ZA<int>(1, "Anna", "Blé", forme);
-    std::cout << "ZA parcelle: " << parcelle1 << " numéro " << parcelle1.getNumero() << " propriétaire " << parcelle1.getProprietaire() << " forme " << parcelle1.getForme() << " type de culture " << parcelle1.getTypeCulture() << std::endl;
+    std::cout << "Parcelle de type : " << parcelle1 << " numéro : " << parcelle1.getNumero() << " propriétaire : " << parcelle1.getProprietaire() << " forme : " << parcelle1.getForme() << " type de culture : " << parcelle1.getTypeCulture() << std::endl;
+    
 
     //Tests du setter et du getter
     parcelle1.setTypeCulture("Maïs");
-    std::cout << "Après modification, type de culture : " << parcelle1.getTypeCulture() << std::endl;
+    std::cout << "Après modification, le type de culture est " << parcelle1.getTypeCulture() << std::endl;
 
     return;
 }

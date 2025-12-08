89dc56acfee6f709d455f582bacdee3f5698b505
Jaaackoo <jack.hascoet@gmail.com>
2025-12-08 14:09:56 +0100

    refact carte

--- src/Carte.hpp
+++ src/Carte.hpp
@@ -8,6 +8,7 @@
  * @brief La carte représente le plan cadastre
  *
  */
+template <typename T>
 class Carte
 {
 private:
@@ -15,7 +16,7 @@ private:
      * @brief Liste des parcelles constituant la carte
      *
      */
-    std::vector<Parcelle *> listeParcelle;
+    std::vector<Parcelle<T> *> listeParcelle;
 
     /**
      * @brief Surface total de la carte
@@ -28,34 +29,50 @@ public:
      * @brief Construct a new Carte object
      *
      */
-    Carte::Carte(void);
+    Carte::Carte(void)
+    {
+        this->listeParcelle = {};
+        this->surface = 0;
+    }
 
     /**
      * @brief Construit une carte à partir d'une liste de parcelles
      *
      * @param lParcelle Liste de parcelles
      */
-    Carte::Carte(std::vector<Parcelle *> lParcelle);
+    Carte::Carte(std::vector<Parcelle<T> *> lParcelles)
+    {
+        this->listeParcelle = lParcelles;
+        this->surface = 0; // Faut sommer la taille des parcelles
+    }
 
     /**
      * @brief Construit une carte à partir d'un fichier
      *
      * @param file Nom du fichier
      */
-    Carte::Carte(std::string file);
+    Carte::Carte(std::string file)
+    {
+        throw std::runtime_error("N'est pas implémenté");
+    }
 
     /**
      * @brief Destroy the Carte object
      *
      */
-    Carte::~Carte(void);
+    Carte::~Carte(void)
+    {
+    }
 
     /**
      * @brief Sauvegarde une carte dans un fichier
      *
      * @param file Nom du fichier
      */
-    void Carte::save(std::string file);
+    void Carte::save(std::string file)
+    {
+        throw std::runtime_error("N'est pas implémenté");
+    }
 };
 
 #endif

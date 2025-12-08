6a41cad27d6af1a993745eeb27397c98ad1d7934
Jaaackoo <jack.hascoet@gmail.com>
2025-12-08 15:45:25 +0100

    debut test ZN

--- mainTest.cpp
+++ mainTest.cpp
@@ -17,13 +17,15 @@
 #include "FichierTest.hpp"
 #include "Point2DTest.hpp"
 #include "PolygoneTest.hpp"
+#include "ZnTest.hpp"
 
 typedef void (*testFunc_t)(void);
 
 std::map<std::string, testFunc_t> testMap = {
     {"Fichier", FichierTest},
     {"Point2D", Point2DTest},
-    {"Polygone", PolygoneTest}
+    {"Polygone", PolygoneTest},
+    {"ZnTest", ZnTest}
 };
 
  int main(int argc, char const *argv[])
---
+++ test/ZnTest.cpp
@@ -0,0 +1,6 @@
+#include "ZnTest.hpp"
+
+void ZnTest(void)
+{
+    std::cout << "abc" << std::endl;
+}
